cc9678549f19b020a8f39eb9eb2f532d75604062
Ben <lenoxbe@appstate.edu>
2024-01-30 15:56:01 -0500

    Database migrations and views

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    if not request.user.is_authenticated:
+        return redirect("login")
+    return render(request, "index.html", context=None)
+
+def login(request):
+    return render(request, "login.html", context=None)
